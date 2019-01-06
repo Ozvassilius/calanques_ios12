bca0ef022ab9a89d765beb7cd7abe002e0c2b0ce
Macinstosh <chhnsn@macbook-air-de-macinstosh.local>
2019-01-06 13:50:22 +0100

    Session 129 - Ajout des annotations sur la carte, avec contraintes dans le code

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -9,21 +9,40 @@
 import UIKit
 import MapKit
 
-class ControllerAvecCarte: UIViewController {
+class ControllerAvecCarte: UIViewController, MKMapViewDelegate {
     
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var calanques : [Calanque] = CalanqueCollection().all()
+    
+    func addAnnotations() {
+        for calanque in calanques {
+            
+            
+            let annotation = MonAnnotation(calanque)
+            mapView.addAnnotation(annotation)
+            
+            // annotation de base (sans le mode MonAnnotation
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = calanque.coordonnee
+//            annotation.title = calanque.nom
+//            mapView.addAnnotation(annotation)
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        mapView.delegate = self
+        addAnnotations()
         
     }
     
     @IBAction func segmentedChange(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
-        case 0:
-            mapView.mapType = MKMapType.standard
+        case 0: mapView.mapType = MKMapType.standard
         case 1: mapView.mapType = .satellite
         case 2: mapView.mapType = .hybrid
         default:
@@ -34,5 +53,36 @@ class ControllerAvecCarte: UIViewController {
     @IBAction func getPosition(_ sender: Any) {
     }
    
+    
+    // Delegate de MapKit
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "reuseID"
+        
+        // verifier que ce soit pas la position de l'utilisateur
+        if annotation.isKind(of: MKUserLocation.self) { return nil }
+        
+        // verifier si le parametre recu annotation est de type MonAnnotation
+        // on cree une variable pour dequeue, pour pas surcharger la memoire
+        // comme pour les tableView
+        if let anno = annotation as? MonAnnotation {
+            
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+            
+            if annotationView == nil { // si l'annotation n'est pas encore crée
+                
+                annotationView = MonAnnotationView(annotation: anno, reuseIdentifier: reuseIdentifier)
+                
+                // Methode de base
+//                annotationView = MKAnnotationView(annotation: anno, reuseIdentifier: reuseIdentifier)
+//                annotationView?.image = UIImage(named: "placeholder" )
+//                annotationView?.canShowCallout = true // bulle
+                return annotationView
+            } else { // cas ou elle est deja crée
+                return annotationView
+            }
+        }
+        return nil
+    }
     
 }
